0d92422841089c4dffc426025d31fa71e983a986
AdamSBlack <18472768+AdamSBlack@users.noreply.github.com>
2022-09-27 22:19:01 +0100

    removed redundant code

--- Core/Src/stm32f4xx_it.c
+++ Core/Src/stm32f4xx_it.c
@@ -24,7 +24,6 @@
 
 const uint8_t crc_poly = 0xD5U; // CRC8
 /* USER CODE BEGIN PFP */
-static uint8_t crc_checksum(uint8_t *dat, int len, const uint8_t poly);
 
 /* USER CODE END PFP */
 
@@ -228,26 +227,6 @@ void CAN1_TX_IRQHandler(void)
 }
 
 
-
-
-// from main.c too
-
-uint8_t crc_checksum(uint8_t *dat, int len, const uint8_t poly) {
-  uint8_t crc = 0xFF;
-  int i, j;
-  for (i = len - 1; i >= 0; i--) {
-    crc ^= dat[i];
-    for (j = 0; j < 8; j++) {
-      if ((crc & 0x80U) != 0U) {
-        crc = (uint8_t)((crc << 1) ^ poly);
-      }
-      else {
-        crc <<= 1;
-      }
-    }
-  }
-  return crc;
-}
 
 
 
